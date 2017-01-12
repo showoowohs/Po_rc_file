62d814fd8f38bbf59ec90a1222697b5aa05eb145
benson <showoowohs@gmail.com>
2017-01-12 14:42:42 +0800

    modify zsh/.oh-my-zsh/lib/git.zsh, 移動到 git 目錄時不會一直搜尋, 避免 lag 問題

--- zsh/.oh-my-zsh/lib/git.zsh
+++ zsh/.oh-my-zsh/lib/git.zsh
@@ -1,11 +1,16 @@
 # Outputs current branch info in prompt format
-function git_prompt_info() {
+function git_prompt_info___old() {
   local ref
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
     echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
+}
+
+function git_prompt_info() {
+   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}${ZSH_THEME_GIT_PROMPT_CLEAN}${ZSH_THEME_GIT_PROMPT_SUFFIX}"
 }
 
 # Checks if working tree is dirty
